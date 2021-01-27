c7e7c81dd37c3922aef27c9cd89b7727467cec15
gustavonvp <gustavonvp@gmail.com>
2021-01-27 12:12:22 -0300

    React + API GitHub

---
+++ modulo05/src/routes.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
